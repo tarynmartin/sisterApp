5627166e2a8f817b704d47ecfbf3c220fc0c8ae9
Taryn Martin <taryn.martin919@gmail.com>
2021-03-24 09:07:58 -0600

    Add label for DOB and first option in dropdown

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -13,8 +13,10 @@ function Signup() {
       <input type="text" placeholder="First Name"></input>
       <input type="text" placeholder="Last Name"></input>
       <input type="email" placeholder="Your Email"></input>
-      <input type="date"></input>
+      <label htmlFor="dob">Date of Birth</label>
+      <input type="date" id="dob"></input>
       <select className="dropdown" onChange={setIDType}>
+        <option>Select ID Type</option>
         <option value="ssn">SSN/EIN</option>
         <option value="passport">Passport</option>
         <option value="id">License/ID</option>
